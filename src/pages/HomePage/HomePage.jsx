4fa49ec5ef0dacce581011483047d4934e975815
IgBovt <140014814+IgBovt@users.noreply.github.com>
2025-08-17 20:03:27 +0300

    add fonts

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,10 @@
+import css from './HomePage.module.css';
 export default function HomePage() {
-  return <h1>Home1</h1>;
+  return (
+    <div>
+      <h1 className={css.text}>Home</h1>
+      <h1 className={css.text2}>Home</h1>
+      <h1>Home</h1>
+    </div>
+  );
 }
