e6941caf2a6fc42f06c960dac4bbb4717c3aa9ef
IgBovt <140014814+IgBovt@users.noreply.github.com>
2025-08-31 18:02:15 +0300

    Update ProjectItem.jsx

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -4,9 +4,9 @@ export default function ProjectItem({ name }) {
   return (
     <img
       className={css.img}
-      src={`/public/desktop/${name}.png`}
+      src={`.desktop/${name}.png`}
       srcSet={`
-        /public/desktop/${name}@2x.png 2x,`}
+        /desktop/${name}@2x.png 2x,`}
       alt="project icon"
     />
   );
