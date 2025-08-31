4dcec32cabd6048113de473eb7b4147b67a44716
IgBovt <140014814+IgBovt@users.noreply.github.com>
2025-08-31 18:04:00 +0300

    fixed problem with build

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -4,9 +4,9 @@ export default function ProjectItem({ name }) {
   return (
     <img
       className={css.img}
-      src={`.desktop/${name}.png`}
+      src={`/${name}.png`}
       srcSet={`
-        /desktop/${name}@2x.png 2x,`}
+        /${name}@2x.png 2x,`}
       alt="project icon"
     />
   );
