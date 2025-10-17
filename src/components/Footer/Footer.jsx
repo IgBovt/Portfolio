66cebb3c04ac3950ed0baeddfdd50a505dfd89b3
IgBovt <140014814+IgBovt@users.noreply.github.com>
2025-10-17 15:48:12 +0300

    fixed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import css from './Footer.module.css';
-
+import { useState } from 'react';
 import Icon from '../../shared/Icon/Icon';
 import { clsx } from 'clsx';
+import Modal from '../../shared/Modal/Modal';
 
 export default function Footer({ isActive }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const mail = 'bovt.anastasia.ux@gmail.com';
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(mail)
+      .then(() => {
+        setIsOpen(true);
+        setTimeout(() => setIsOpen(false), 2000);
+      })
+      .catch(err => {
+        console.error('Помилка копіювання:', err);
+      });
+  };
+
   return (
     <footer className={clsx(css.container, isActive && css.active)}>
       <div className={css.contentContainer}>
@@ -14,14 +31,27 @@ export default function Footer({ isActive }) {
           design systems, and business analysis, with a strong focus on
           requirements definition and aligning design with business goals.
         </p>
-        <a className={css.cvLink} href="/example.pdf" download="CV">
+        <a className={css.cvLink} href="/example.pdf" download="CV-Bovt">
           Download CV
         </a>
         <div className={css.mailContainer}>
-          <p className={css.mail}>bovt.anastasia.ux@gmail.com</p>
+          <div className={css.contacts}>
+            <p className={css.mail} onClick={handleCopy}>
+              bovt.anastasia.ux@gmail.com
+            </p>
+            <a
+              className={css.link}
+              href="https://www.linkedin.com/in/bovt-anastasia/"
+              target="_blank"
+            >
+              Linkidin
+            </a>
+          </div>
+
           <Icon className={css.iconArrow} id={'up'} />
         </div>
       </div>
+      <Modal isOpen={isOpen} />
     </footer>
   );
 }
